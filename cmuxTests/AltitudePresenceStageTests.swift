f0432a18a117c2c849ec3e9e680951de2505fba4
xianxing <xianxing@papercut-labs.com>
2026-08-01 16:59:34 +0800

    Fix Altitude test module import

--- cmuxTests/AltitudePresenceStageTests.swift
+++ cmuxTests/AltitudePresenceStageTests.swift
@@ -1,6 +1,11 @@
 import Foundation
 import Testing
+
+#if canImport(cmux_DEV)
+@testable import cmux_DEV
+#elseif canImport(cmux)
 @testable import cmux
+#endif
 
 @Suite("Altitude presence stages")
 struct AltitudePresenceStageTests {
